c65c15f7f6cab20b52849d2c6d806ca7753d2d1c
Franco M <damianmaciel0@gmail.com>
2022-07-20 17:54:19 -0300

    add form sign in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-
 import style from "./App.module.css";
 import Nav from "./components/Nav/Nav"
 import AccountDetail from './components/AccountDetail/AccountDetail';
-
-
+import { Route, Routes } from 'react-router-dom'
+import SignIn from "./components/account/signin";
+import Login from "./components/account/login";
 function App () {
 
   return (
-
     <div className={style.container}>
-      <Nav />
-      <AccountDetail />
+      <Routes>
+      <Route path="/account/signin" element={<SignIn />} />
+      <Route path="/account/login" element={<Login />} />
+        <Route path="/" element={
+        <>
+          <Nav />
+          <AccountDetail />
+        </>
+        } />
+      </Routes>
     </div>
   );
 }
