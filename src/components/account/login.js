b9965a4acb6877293839e5ebc7880ddddea8ddeb
Franco M <damianmaciel0@gmail.com>
2022-07-21 15:43:56 -0300

    add Login with Google

--- src/components/account/login.js
+++ src/components/account/login.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
+import { LOG_IN } from '../../services/LOG_IN'
+import formStyles from './form.module.css'
+import { LoginWithGoogle } from '../../firebase_/client'
 
 const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const onSubmit = async ({ email, password }) => {
+    LOG_IN({ email, password }).then(console.log).catch(console.error)
+  }
+  const login = () => {
+    LoginWithGoogle().then(({ user }) => {
+      console.log(user)
+      LOG_IN({ googleID: user.uid }).then(console.log).catch(console.error)
+    }).catch(console.error)
+  }
   return (
-    <div>Login</div>
+    <div className={`${formStyles.center} ${formStyles.min_h_100vh}`}>
+
+      <div className={formStyles.card}>
+        <form onSubmit={handleSubmit(onSubmit)} className={formStyles.form}>
+          <div className={formStyles.input_container}>
+            <input className={formStyles.input_text} defaultValue="" placeholder='email' {...register('email', { required: true, minLength: 8 })} />
+            {errors.email && <span className={formStyles.input_error}>This field is required</span>}
+          </div>
+          <div className={formStyles.input_container}>
+            <input className={formStyles.input_text} defaultValue="" placeholder='password' {...register('password', { required: true, minLength: 8 })} />
+            {errors.password && <span className={formStyles.input_error}>his field is required</span>}
+          </div>
+          <div className={formStyles.center}>
+            <button className={`${formStyles.button} ${formStyles.button_submit}`} type='submit'>Log in</button>
+          </div>
+        </form>
+        <div className={formStyles.center}>
+          <button className={`${formStyles.button} ${formStyles.button_google}`} onClick={login}>Login with Google</button>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/services/LOG_IN.js
@@ -0,0 +1,17 @@
+export const LOG_IN = async ({ email, password, googleID }) => {
+    console.log({ email, password })
+    try {  
+      const res = await fetch('http://localhost:4000/api/user/login', {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        method: 'POST',
+        body: JSON.stringify({ email, password, googleID })
+      })
+      const resJSON = await res.json()
+      return resJSON
+    } catch (error) {
+      console.error({ error })
+    }
+  }
+  
