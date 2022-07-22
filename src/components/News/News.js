515fb0073bd7dba4786fa2d6a0abd0159a15b638
Matt <maty.britez7@gmail.com>
2022-07-22 12:27:12 -0300

    add

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,7 +5,13 @@ import styles from './News.module.css'
 
 const NewsComponent = () => {
   const [news, setNews] = useState([])
+  const [riesgo, setRiesgo] = useState([])
 
+  const getRP = async () => {
+    const info = await axios.get('http://localhost:4000/api/currency/riesgopais')
+    const data = info.data
+    setRiesgo(data)
+  }
   const getNews = async () => {
     const info = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=66b54c0b1d0444a48de1291d57f5e137')
     const data = info.data.articles
@@ -14,12 +20,13 @@ const NewsComponent = () => {
   
   useEffect(() => {
     getNews()
+    getRP()
   }, []);
   console.log(news)
   
   return (
     <div className={styles.boxContainer}>
-      <h1>News</h1>
+      <h1>News</h1><h2>{riesgo.value}</h2>
       <div className={styles.columns_3_2_1}>
       { news.map((news) => 
         <div className={styles.card_news} key={news.title}>
