d9937268603e30db2ca77cb613efecfc233afb7b
andresDiazLugo <andresd.ad5@gmail.com>
2022-07-22 11:46:46 -0300

    change/profile/ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import style from "./App.module.css";
 import Landing from "./components/Landing/Landing";
-import Nav from "./components/Nav/Nav"
-import AccountDetail from './components/AccountDetail/AccountDetail';
 import {Routes,Route} from 'react-router-dom'
 import Profile from "./components/Profile/Profile";
-import DetailProfile from "./components/Profile/DetailProfile";
 import SignIn from "./components/account/signin";
 import Login from "./components/account/login";
 import Cryptos from './pages/Cryptos/Cryptos'
@@ -13,7 +10,7 @@ import Wallet from './pages/Wallet/Wallet';
 import Settings from "./pages/Settings/Settings";
 import News from "./pages/News/News";
 import Faq from "./pages/Faq/Faq";
-import EditProfile from "./components/Profile/EditProfile";
+
 
 
 
@@ -23,11 +20,9 @@ function App () {
     <div className={style.container}>
       <Routes>
         <Route path="/" element={<Landing/>}/>
-        <Route path="/perfil" element={<Profile/>}/>
-        <Route path="/perfil/detail" element={<DetailProfile/>}/>
+        <Route path="/profile" element={<Profile/>}/>
         <Route path="/account/signin" element={<SignIn />} />
         <Route path="/account/login" element={<Login />} />
-        <Route path="/perfil/detail/editProfile/:id" element={<EditProfile/>}/>
         <Route path="/home" element={
           <div className={style.app_container}>
             <Home />
