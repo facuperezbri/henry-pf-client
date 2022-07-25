a7c08c8e0d1784b70c5e81f891b26956ea73498e
Diego Alonso <diegoalonso.gutierrez@outlook.com>
2022-07-25 16:57:00 -0500

    update news

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,13 +5,13 @@ import styles from './News.module.css'
 
 const NewsComponent = () => {
   const [news, setNews] = useState([])
-  const [riesgo, setRiesgo] = useState([])
+  // const [riesgo, setRiesgo] = useState([])
 
-  const getRP = async () => {
-    const info = await axios.get('http://localhost:4000/api/currency/riesgopais')
-    const data = info.data
-    setRiesgo(data)
-  }
+  // const getRP = async () => {
+  //   const info = await axios.get('http://localhost:4000/api/currency/riesgopais')
+  //   const data = info.data
+  //   setRiesgo(data)
+  // }
   const getNews = async () => {
     const info = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=66b54c0b1d0444a48de1291d57f5e137')
     const data = info.data.articles
@@ -20,7 +20,7 @@ const NewsComponent = () => {
 
   useEffect(() => {
     getNews()
-    getRP()
+    // getRP()
   }, []);
 
   const [local, setLocal] = useState(1)
@@ -33,7 +33,7 @@ const NewsComponent = () => {
   }
 
   let firstPage = [];
-  let elemsByPage = 8
+  let elemsByPage = 10
   for (let i=1; i <= Math.ceil(news.length/elemsByPage); i++) {
       firstPage.push(i)
   }
@@ -43,32 +43,21 @@ const NewsComponent = () => {
   
   const pages = firstPage.map((elem) => {
       return <li
-      className={styles.pageNumber}
+      className={local === elem ? styles.pageSelected : styles.pageUnselected}
       key={elem}
       id={elem} 
       onClick={changeState} 
       >
-          {elem}
-          {/* <img
-          className={style.imagen}
-          id={elem}
-          src={local === elem ? abierta : cerrada}
-          alt="what?"
-          >
-          </img> */}
+        {elem}    
+        {/* <img
+        className={style.imagen}
+        id={elem}
+        src={local === elem ? abierta : cerrada}
+        alt="what?"
+        >
+        </img> */}
       </li>
   })
-
-
-
-
-
-
-
-
-
-
-
 
   return (
     <div className={styles.boxContainer}>
