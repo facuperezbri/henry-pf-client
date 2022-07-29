25d8d96893beaac4f2b7b556baa7c9b84e5f5631
Matt <maty.britez7@gmail.com>
2022-07-28 22:32:02 -0300

    dinamic

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,26 +4,32 @@ import { GET_USER_DATA } from "../../services/GET_USER_DATA";
 import { Link } from "react-router-dom";
 import image from "../../assets/img/image.jpg";
 import pen from '../../assets/icons/pen.svg'
+import { useSelector } from 'react-redux'
+
 export default function Profile() {
-  // const [dataProfile,setProfile] = useState("")
-  //     useEffect(()=>{
-  //  GET_USER_DATA(window.localStorage.getItem("token"))
-  //  .then(data => setProfile(data))
-  //   },[])
+   const [dataProfile,setProfile] = useState("")
+   const globalData = useSelector(state => state.userData)
+
+       useEffect(()=>{
+    GET_USER_DATA(window.localStorage.getItem("token"))
+    .then(data => setProfile(data))
+     },[])
 
   return (
     <div className={Style.main}>
-      <div className={Style.container}>
-        <img className={Style.img} src={image} />
-        <button><img src={pen} alt="pen edit" className={Style.pen}/></button>
-
+     {dataProfile ? <div className={Style.container}>
+        <img className={Style.img} src={dataProfile?.profilepic} alt='pic'/>
+        <button><img src={pen} alt="pen edit" className={Style.pen}/></button> 
       </div>
-      <div className={Style.containerDetail}>
+        : <h1>Loading...</h1>
+     }
+
+{dataProfile ? <div className={Style.containerDetail}>
         <div className={Style.containerName}>
           <div>
           <label>
             First name
-            <input type="text" value="matt" readonly className={Style.input} />
+            <input type="text" value={dataProfile.name} readonly className={Style.input} />
           </label>
           </div>
           <div>
@@ -31,7 +37,7 @@ export default function Profile() {
             Last name
             <input
               type="text"
-              value="britez"
+              value={dataProfile.lastname}
               readonly
               className={Style.input}
             />
@@ -42,20 +48,23 @@ export default function Profile() {
             Email
             <input
               type="text"
-              value="britez.maty@gmail.com"
+              value={dataProfile.email}
               readonly
               className={Style.input}
-            />
+            /> 
+        
           </label></div> <div> <label>
             UserName
             <input
               type="text"
-              value="britezmatt"
+              value={dataProfile.username}
               readonly
               className={Style.input}
             />
-          </label><button><img src={pen} alt="pen edit" className={Style.pen}/></button></div>
+          </label><button><img src={pen} alt="pen edit" className={Style.pen2}/></button></div>
       </div>
+      : null
+    }
      
         <button>Change password</button>
       
