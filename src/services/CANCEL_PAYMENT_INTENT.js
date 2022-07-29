74b76d41c18593c84a12b5d4e3e5bbf9bb7c2a13
Franco M <damianmaciel0@gmail.com>
2022-07-29 13:43:12 -0300

    fix stripe auth

--- src/services/CANCEL_PAYMENT_INTENT.js
+++ src/services/CANCEL_PAYMENT_INTENT.js
@@ -1,11 +1,17 @@
 import { API_URL } from "./API"
+import { useToken } from "../hooks/useToken"
 
 export const CANCEL_PAYMENT_INTENT = async (paymentIntentID) => {
+    const { token } = useToken()
+
     try {
         const res = await fetch(`${API_URL}/api/movement/cancel_payment_intent`, {
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
             method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ paymentIntentID }),
+            body: JSON.stringify({ paymentIntentID })
         })
         const resJson = await res.json()
         return resJson
