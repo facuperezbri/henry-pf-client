1b2e61fc9dac49808a07581d59c1d2878265910b
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-08-01 08:34:16 -0300

    update

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react'
 import style from './Nav.module.css'
 import { Link } from "react-router-dom"
 
-import Dashboard from '../../assets/icons/Sidebar/Dashboard.svg'
-import FAQ from '../../assets/icons/Sidebar/FAQ.svg'
+import { MdOutlineSpaceDashboard, MdExitToApp } from 'react-icons/md'
+import { FiHelpCircle } from 'react-icons/fi'
 import Logout from '../../assets/icons/Sidebar/Logout.svg'
-import News from '../../assets/icons/Sidebar/News.svg'
-import Settings from '../../assets/icons/Sidebar/Settings.svg'
-import Wallet from '../../assets/icons/Sidebar/Wallet.svg'
-import Profile from '../../assets/icons/Sidebar/Profile.svg'
-import full from '../../assets/img/estrellaLlena.png'
+import { IoNewspaperOutline, IoSettingsOutline } from 'react-icons/io5'
+import { BsWallet2 } from 'react-icons/bs'
+import { CgProfile } from 'react-icons/cg'
+import { AiOutlineStar } from 'react-icons/ai'
 import RateForm from './RateForm'
 import { openRate } from '../../redux/actions'
 import { useDispatch, useSelector } from 'react-redux'
@@ -44,36 +43,36 @@ export default function Nav () {
         <ul className={style.itemsNav}>
           <Link to='/profile'>
             <li className={style.listItem}>
-              <img src={Profile} alt='Profile' /> <span className={style.listItem_text}>Profile</span>
+              <CgProfile className={style.icon} /> <span className={style.listItem_text}>Profile</span>
             </li>
           </Link>
           <Link to='/home'>
             <li className={style.listItem}>
-              <img src={Dashboard} alt='Dashboard' /> <span className={style.listItem_text}>Dashboard</span>
+              <MdOutlineSpaceDashboard className={style.icon} /> <span className={style.listItem_text}>Dashboard</span>
             </li>
           </Link>
           <Link to='/wallet'>
             <li className={style.listItem}>
-              <img src={Wallet} alt='Wallet' /><span className={style.listItem_text}>Wallet</span>
+              <BsWallet2 className={style.icon} /><span className={style.listItem_text}>Wallet</span>
             </li>
           </Link>
           <Link to='/news'>
             <li className={style.listItem}>
-              <img src={News} alt='News' /><span className={style.listItem_text}>News</span>
+              <IoNewspaperOutline className={style.icon} /> <span className={style.listItem_text}>News</span>
             </li>
           </Link>
           <Link to='/settings'>
             <li className={style.listItem}>
-              <img src={Settings} alt='Settings' /> <span className={style.listItem_text}>Settings</span>
+              <IoSettingsOutline className={style.icon} /> <span className={style.listItem_text}>Settings</span>
             </li>
           </Link>
           <Link to='/faq'>
             <li className={style.listItem}>
-              <img src={FAQ} alt='FAQ' /><span className={style.listItem_text}>F.A.Q</span>
+              <FiHelpCircle className={style.icon} /><span className={style.listItem_text}>F.A.Q</span>
             </li>
           </Link>
           <li onClick={openRateclick} className={style.listItem}>
-            <img src={full} alt='Rate' className={style.icon} /><span className={style.listItem_text}>Rate us!</span>
+            <AiOutlineStar className={style.icon} /><span className={style.listItem_text}>Rate us!</span>
           </li>
           {/* {
             (function show () {
@@ -92,7 +91,7 @@ export default function Nav () {
       <ul className={style.itemsNav}>
         <Link to="/">
           <li onClick={() => logOut()} className={style.listItem}>
-            <img src={Logout} alt='Log out' /> <span className={style.listItem_text}>Log out</span>
+            <MdExitToApp className={style.icon} /> <span className={style.listItem_text}>Log out</span>
           </li>
         </Link>
       </ul>
