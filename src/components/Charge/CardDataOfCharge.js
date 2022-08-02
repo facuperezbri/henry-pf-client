0068e30aa628ef3a833de4aab45113f1f2b3bcdb
Franco M <damianmaciel0@gmail.com>
2022-08-02 13:23:34 -0300

    add more data of user to charge

--- src/components/Charge/CardDataOfCharge.js
+++ src/components/Charge/CardDataOfCharge.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const CardDataOfCharge = ({ cvu, amount, method }) => {
+const CardDataOfCharge = ({ cvu, amount, method, userToCharge }) => {
   return (
     <div>
     <div>
+        <div><span>DNI:</span> {userToCharge?.dni}</div>
+        <div><span>Name:</span>{userToCharge?.name}</div>
+        <div><span>Lastname:</span>{userToCharge?.lastname}</div>
+        <div><span>Username:</span>{userToCharge?.username}</div>
+        <div><img src={userToCharge?.profilepic} alt={userToCharge?.username} /></div>
+
         <div>
             <span>{cvu}</span>
         </div>
