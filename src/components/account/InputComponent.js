257a24f4c193313eb3d2243cd4c3b38375d01691
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-08-03 21:57:34 -0300

    input component

--- src/components/account/InputComponent.js
+++ src/components/account/InputComponent.js
@@ -2,8 +2,8 @@ import formStyles from './form.module.css'
 
 export const InputComponent = ({ register, errors, type, placeholder, name, config }) => {
     return (
-        <div className={formStyles.input_container}>
-            <input className={formStyles.input_text} type={type} placeholder={placeholder} {...register(name, config)} />
+        <div className="flex flex-wrap -mx-3 mb-6">
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white" type={type} placeholder={placeholder} {...register(name, config)} />
             {errors[name] && <span className={formStyles.input_error}>This field is required</span>}
         </div>
     )
