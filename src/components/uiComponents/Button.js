a8c9694e942adf726c13831dba4b97bf5c7e4733
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-08-04 14:52:50 -0300

    update

--- src/components/uiComponents/Button.js
+++ src/components/uiComponents/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ children, onClick }) => {
+const Button = ({ children, onClick, type }) => {
   return (
-    <button onClick={onClick} className="bg-primary-red text-white w-fit h-12 border-none px-3 rounded-md font-semibold text-lg cursor-pointer z-10 overflow-hidden">
+    <button onClick={onClick} type={type || 'button'} className="bg-primary-red text-white w-fit h-12 border-none px-3 rounded-md font-semibold text-lg cursor-pointer z-10 overflow-hidden">
       {children}
     </button>
   )
--- src/components/uiComponents/InputComponent.js
+++ src/components/uiComponents/InputComponent.js
@@ -1,10 +1,10 @@
 // import formStyles from '../account/form.module.css'
 
-const InputComponent = ({ register, errors, type, placeholder, name, config, labeltext, msgerror }) => {
+const InputComponent = ({ register, errors, type, placeholder, name, config, labeltext, msgerror, min }) => {
     return (
-        <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="flex flex-wrap -mx-3 mb-6 w-full">
             {labeltext && <label>{labeltext}</label>}
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white" type={type} placeholder={placeholder} {...register(name, config)} />
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white" type={type} placeholder={placeholder} min={min} {...register(name, config)} />
             {errors[name] && <span >{msgerror}</span>}
         </div>
     )
