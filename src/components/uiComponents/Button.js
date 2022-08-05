ae799d1f924b2d9ce3dde89b5ec8177fe7cef990
Franco M <damianmaciel0@gmail.com>
2022-08-05 14:49:25 -0300

    add darkmode

--- src/components/uiComponents/Button.js
+++ src/components/uiComponents/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ children, onClick, type }) => {
   return (
-    <button onClick={onClick} type={type || 'button'} className="bg-primary-red text-white w-fit h-12 border-none px-3 rounded-md font-semibold text-lg cursor-pointer z-10 overflow-hidden">
+    <button onClick={onClick} type={type || 'button'} className="bg-primary-red text-white w-fit h-12 border-none px-3 rounded-md font-semibold text-lg cursor-pointer">
       {children}
     </button>
   )
--- src/components/uiComponents/InputComponent.js
+++ src/components/uiComponents/InputComponent.js
@@ -4,7 +4,7 @@ const InputComponent = ({ register, errors, type, placeholder, name, config, lab
     return (
         <div className="flex flex-wrap mb-6 w-full">
             {labeltext && <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">{labeltext}</label>}
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white" type={type} placeholder={placeholder} min={min} {...register(name, config)} />
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-slate-900 border rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white" type={type} placeholder={placeholder} min={min} {...register(name, config)} />
             {errors[name] && <span >{msgerror}</span>}
         </div>
     )
