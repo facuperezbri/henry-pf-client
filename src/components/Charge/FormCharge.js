95e9f852f925a6ab80a4841dffb9368dee25b4c1
Franco M <damianmaciel0@gmail.com>
2022-08-08 17:12:33 -0300

    add styles to charge page

--- src/components/Charge/FormCharge.js
+++ src/components/Charge/FormCharge.js
@@ -1,20 +1,23 @@
 import { useForm } from 'react-hook-form'
+import Button from '../uiComponents/Button'
 import { CREATE_PAYMENT_INTENT } from '../../services/CREATE_PAYMENT_INTENT'
+import Card from '../uiComponents/Card'
+import InputComponent from '../uiComponents/InputComponent'
+import CardText from '../uiComponents/CardText'
 const METHODS = ['PAGO RAPIDO', 'FACIL PAGO']
 
 
-const FormOfCharge = ({ setDataOfCharge, setClientSecret, setUserToCharge }) => {
+const FormOfCharge = ({ setDataOfCharge, setClientSecret, CVU }) => {
 
     const { register, handleSubmit, formState: { errors } } = useForm()
     const onPaymentIntent = (data) => {
         if (data.amount.includes(',')) {
             return alert('Incorrect format amount')
         }
-        CREATE_PAYMENT_INTENT(data.amount, data.cvu).then((res) => {
+        CREATE_PAYMENT_INTENT(data.amount, CVU).then((res) => {
             if (res?.msg) {
                 return alert(res?.msg)
             }
-            setUserToCharge(res?.account?.users)
             setClientSecret(res.clientSecret)
             setDataOfCharge({ ...data, paymentIntentID: res?.paymentIntentID })
         })
@@ -22,11 +25,13 @@ const FormOfCharge = ({ setDataOfCharge, setClientSecret, setUserToCharge }) => 
     const MIN_CHARGE = 300
     const MAX_CHARGE = 1000000
     return (
-        <form onSubmit={handleSubmit(onPaymentIntent)}>
+        <Card>
 
-            <div>
-                <label htmlFor="cars">Choose a car:</label>
-                <select id="cars"  {...register('chargeMethod', { required: true })}>
+        <form onSubmit={handleSubmit(onPaymentIntent)} className='flex flex-col gap-4'>
+
+            <CardText>
+                <label htmlFor="cars">Choose a charge method:</label>
+                <select id="cars" className='dark:text-stone-800' {...register('chargeMethod', { required: true })}>
                     {
                         METHODS.map((method) => (
                             <option key={method} value={method}>{method}</option>
@@ -35,24 +40,23 @@ const FormOfCharge = ({ setDataOfCharge, setClientSecret, setUserToCharge }) => 
                 </select>
                 {errors.chargeMethod && <span>This is required</span>}
 
-            </div>
+            </CardText>
             <div>
-                <label>
-                    <span>CVU:</span>
-                    <input type="text" name="cvu" {...register('cvu', { required: true, minLength: 8 })} />
-                    {errors.cvu && <span>CVU is required and min length must be 8.</span>}
-
-                </label>
-                <label>
+                {/* <label>
                     <span>Amount:</span>
-                    <input type="number" {...register('amount', { required: true, min: MIN_CHARGE, max: MAX_CHARGE })} min={MIN_CHARGE} max={MAX_CHARGE} step="0.01"></input>
+                    <input type="number" {...register('amount', { required: true, min: MIN_CHARGE, max: MAX_CHARGE })} min={MIN_CHARGE} max={MAX_CHARGE} step="0.01" />
                     {errors.amount && <span>{`The Amount is required and must be between ${MIN_CHARGE} and ${MAX_CHARGE}`}</span>}
-                </label>
+                </label> */}
+                <InputComponent labeltext='Amount' register={register} errors={errors} msgerror={`The Amount is required and must be between ${MIN_CHARGE} and ${MAX_CHARGE}`} name='amount' placeholder='Your password' type='number' config={{ required: true, min: MIN_CHARGE, max: MAX_CHARGE }} min={MIN_CHARGE} max={MAX_CHARGE} step="0.01" />
+
             </div>
-            <button type="submit">
-                Charge
-            </button>
+            <div className='grid place-content-center'>
+                <Button type="submit">
+                    Charge
+                </Button>
+            </div>
         </form>
+        </Card>
     )
 }
 
