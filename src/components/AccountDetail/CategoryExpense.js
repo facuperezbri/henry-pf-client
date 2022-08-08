73d7e5175de3f39e8476695229fbb1ec7a7ed82e
Diego Alonso <diegoalonso.gutierrez@outlook.com>
2022-08-08 13:08:24 -0500

    refactor graph and cats

--- src/components/AccountDetail/CategoryExpense.js
+++ src/components/AccountDetail/CategoryExpense.js
@@ -1,17 +1,29 @@
 import React from 'react'
-import transport from '../../assets/icons/transport.svg'
-import shopping from '../../assets/icons/shopping.svg'
-import subscriptions from '../../assets/icons/subscriptions.svg'
-import groceries from '../../assets/icons/groceries.svg'
-import charge from '../../assets/icons/groceries.svg'
-import entertainment from '../../assets/icons/groceries.svg'
-import selfcare from '../../assets/icons/groceries.svg'
-import services from '../../assets/icons/groceries.svg'
-import travels from '../../assets/icons/groceries.svg'
 import style from './CategoryExpense.module.css'
+import {TbBatteryCharging} from 'react-icons/tb'
+import {TbMusic} from 'react-icons/tb'
+import {TbShoppingCart} from 'react-icons/tb'
+import {TbPlant2} from 'react-icons/tb'
+import {TbTool} from 'react-icons/tb'
+import {BiShoppingBag} from 'react-icons/bi'
+import {TbTicket} from 'react-icons/tb'
+import {TbCar} from 'react-icons/tb'
+import {TbPlane} from 'react-icons/tb'
+import {TbCheck} from 'react-icons/tb'
 
 
 export default function CategoryExpense ({ activities }) { // los movimientos, con su categoria y monto
+
+    const charge = <TbBatteryCharging/>
+    const entertainment = <TbMusic/>
+    const groceries = <TbShoppingCart/>
+    const selfcare = <TbPlant2/>
+    const services = <TbTool/>
+    const shopping = <BiShoppingBag/>
+    const subscriptions = <TbTicket/>
+    const transport = <TbCar/>
+    const travels = <TbPlane/>
+    const other = <TbCheck/>
 
     let categoriesSum = [
         { name: "Charge", sum: 0, icon: charge },
@@ -22,14 +34,15 @@ export default function CategoryExpense ({ activities }) { // los movimientos, c
         { name: "Shopping", sum: 0, icon: shopping },
         { name: "Subscriptions", sum: 0, icon: subscriptions },
         { name: "Transport", sum: 0, icon: transport },
-        { name: "Travels", sum: 0, icon: travels }
+        { name: "Travels", sum: 0, icon: travels },
+        { name: "Other", sum: 0, icon: other }
     ]
 
     let provSum = 0
 
     for (let i = 0; i < categoriesSum.length; i++) {
         for (let j = 0; j < activities?.length; j++) {
-            if (activities?.[j].categories?.name === categoriesSum[i].name) {
+            if (activities?.[j].categories?.name === categoriesSum[i].name && activities?.[j].operations.name === "Debit") {
                 provSum += activities?.[j].amount
             }
         }
@@ -37,35 +50,38 @@ export default function CategoryExpense ({ activities }) { // los movimientos, c
         provSum = 0
     }
 
-    // console.log(categoriesSum.filter((abc) => abc.sum !== 0))
+    let catWithExpenses = categoriesSum.filter((abc) => abc.sum !== 0)
+
+    catWithExpenses = catWithExpenses.sort((a, b) => {
+        if (a.sum < b.sum) {
+            return 1;
+        }
+        if (a.sum > b.sum) {
+            return -1;
+        }
+        return 0
+    })
 
     return (
         <div>
             <div className={style.categoriesContainer}>
                 <ul className={style.listContainer}>
-
-                    {/* ESTA PRIMERA OPCION ES PARA QUE APAREZCAN SOLO LAS CATEGORIAS UTILIZADAS*/}
-                    {
-                        categoriesSum.filter((abc) => abc.sum !== 0).map((xxx, i) => (
+                    {   catWithExpenses.length <= 4 ?
+                        catWithExpenses.map((xxx, i) => (
                             <li key={i}>
-                                <img src={xxx.icon} alt={`${xxx.name} NF`} />
+                                {xxx.icon}
+                                <div>{xxx.sum}</div>
+                                <div>{xxx.name}</div>
+                            </li>
+                        )) :
+                        catWithExpenses.slice(0,5).map((xxx, i) => (
+                            <li key={i}>
+                                {xxx.icon}
                                 <div>{xxx.sum}</div>
                                 <div>{xxx.name}</div>
                             </li>
                         ))
                     }
-
-                    {/* ESTA SEGUNDA OPCION ES PARA QUE APAREZCAN TODAS LAS CATEGORIAS AUN SI TIENEN CERO*/}
-                    {/* {
-                    categoriesSum.map((xxx) => (
-                        <li>
-                            <img src={xxx.icon} alt={`${xxx.name} NF`}/>
-                            <div>{xxx.sum}</div>
-                            <div>{xxx.name}</div>
-                        </li>
-                    ))
-                } */}
-
                 </ul>
             </div>
         </div>
