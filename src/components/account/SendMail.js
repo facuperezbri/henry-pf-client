dbe4c4b324e6d86e63f21e588bb0fe8a0b2f22cd
Matt <maty.britez7@gmail.com>
2022-08-08 10:11:15 -0300

    changes

--- src/components/account/SendMail.js
+++ src/components/account/SendMail.js
@@ -1,29 +1,39 @@
-import {useState} from 'react'
-import axios from 'axios'
-import styles from './SendMail.module.css'
-import InputComponent from './../uiComponents/InputComponent'
-
+import { useState } from "react";
+import axios from "axios";
+import styles from "./SendMail.module.css";
 
 const SendMail = () => {
-  const [email, setEmail] = useState()
+  const [email, setEmail] = useState();
 
   const send = async () => {
-    const mail = await axios.post('http://localhost:4000/api/user/sendReset', {
-      email
-     })
-     const emaildata = mail.data
-     console.log(emaildata)  
-  }
+    const mail = await axios.post("http://localhost:4000/api/user/sendReset", {
+      email,
+    });
+    const emaildata = mail.data;
+    console.log(emaildata);
+  };
 
   const handleOnChange = (e) => {
-    e.preventDefault()
-    console.log(e.target.value)
-    setEmail(e.target.value)
-  }
+    e.preventDefault();
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
 
   return (
-    <div className={styles.container}><InputComponent placeholder="Enter your email for reset" name={'Reset'}onChange={(e) => handleOnChange(e)} /><button onClick={send} className={styles.btn}>Send</button></div>
-  )
-}
+    <div className={styles.container}>
+      
+      <input
+        className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-slate-900 border rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+        name={"Reset"}
+        placeholder="Enter your email for reset"
+        min= {8}
+        onChange={(e) => handleOnChange(e)}
+      />
+      <button onClick={send} className={styles.btn}>
+        Send
+      </button>
+    </div>
+  );
+};
 
-export default SendMail
+export default SendMail;
