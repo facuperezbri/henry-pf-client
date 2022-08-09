647ebaff74e601c0c6e7e026b4c67e950cac44c2
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-08-09 13:28:48 -0300

    update

--- src/components/AccountDetail/CategoryExpense.js
+++ src/components/AccountDetail/CategoryExpense.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import style from './CategoryExpense.module.css'
-import {TbBatteryCharging} from 'react-icons/tb'
-import {TbMusic} from 'react-icons/tb'
-import {TbShoppingCart} from 'react-icons/tb'
-import {TbPlant2} from 'react-icons/tb'
-import {TbTool} from 'react-icons/tb'
-import {BiShoppingBag} from 'react-icons/bi'
-import {TbTicket} from 'react-icons/tb'
-import {TbCar} from 'react-icons/tb'
-import {TbPlane} from 'react-icons/tb'
-import {TbCheck} from 'react-icons/tb'
+import { TbBatteryCharging } from 'react-icons/tb'
+import { TbMusic } from 'react-icons/tb'
+import { TbShoppingCart } from 'react-icons/tb'
+import { TbPlant2 } from 'react-icons/tb'
+import { TbTool } from 'react-icons/tb'
+import { BiShoppingBag } from 'react-icons/bi'
+import { TbTicket } from 'react-icons/tb'
+import { TbCar } from 'react-icons/tb'
+import { TbPlane } from 'react-icons/tb'
+import { TbCheck } from 'react-icons/tb'
 
 
 export default function CategoryExpense ({ activities }) { // los movimientos, con su categoria y monto
 
-    const charge = <TbBatteryCharging size={60}/>
-    const entertainment = <TbMusic size={60}/>
-    const groceries = <TbShoppingCart size={60}/>
-    const selfcare = <TbPlant2 size={60}/>
-    const services = <TbTool size={60}/>
-    const shopping = <BiShoppingBag size={60}/>
-    const subscriptions = <TbTicket size={60}/>
-    const transport = <TbCar size={60}/>
-    const travels = <TbPlane size={60}/>
-    const other = <TbCheck size={60}/>
+    const charge = <TbBatteryCharging size={60} />
+    const entertainment = <TbMusic size={60} />
+    const groceries = <TbShoppingCart size={60} />
+    const selfcare = <TbPlant2 size={60} />
+    const services = <TbTool size={60} />
+    const shopping = <BiShoppingBag size={60} />
+    const subscriptions = <TbTicket size={60} />
+    const transport = <TbCar size={60} />
+    const travels = <TbPlane size={60} />
+    const other = <TbCheck size={60} />
 
     let categoriesSum = [
         { name: "Charge", sum: 0, icon: charge },
@@ -63,18 +63,18 @@ export default function CategoryExpense ({ activities }) { // los movimientos, c
     })
 
     return (
-            <section >
-                <ul className='flex flex-wrap gap-28 w-full justify-center items-center'>
-                    {   
-                        catWithExpenses.map((xxx, i) => (
-                            <li className='grid place-items-center' key={i}>
-                                {xxx.icon}
-                                <p className='font-bold rounded-lg bg-red-400 my-1 px-1.5'> $ {xxx.sum.toString().slice(0,xxx.sum.toString().length-3)},{xxx.sum.toString().slice(xxx.sum.toString().length-3)}</p>
-                                <p>{xxx.name}</p>
-                            </li>
-                        )) 
-                    }
-                </ul>
-            </section>
+        <section >
+            <ul className='flex flex-wrap gap-8 w-full justify-center items-center md:gap-20'>
+                {
+                    catWithExpenses.map((xxx, i) => (
+                        <li className='grid place-items-center' key={i}>
+                            {xxx.icon}
+                            <p className='font-bold rounded-lg bg-red-400 my-1 px-1.5'> $ {xxx.sum.toString().slice(0, xxx.sum.toString().length - 3)},{xxx.sum.toString().slice(xxx.sum.toString().length - 3)}</p>
+                            <p>{xxx.name}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </section>
     )
 }
