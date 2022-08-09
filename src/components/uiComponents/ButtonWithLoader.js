36ebb7ffcb9ba4223ae9da0a9c54af361e9e565b
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-08-09 13:19:40 -0300

    update

--- src/components/uiComponents/ButtonWithLoader.js
+++ src/components/uiComponents/ButtonWithLoader.js
@@ -3,10 +3,10 @@ import Loader from './Loader'
 
 
 const ButtonWithLoader = ({ children, onClick, type, id, className, isLoading }) => {
-    const classButton = "bg-primary-red text-white h-12 border-none px-3 rounded-md font-semibold text-lg cursor-pointer grid place-content-center"
+    const classButton = "bg-primary-red text-white h-12 border-none px-3 rounded-md font-semibold cursor-pointer grid place-content-center"
 
     return (
-        <button disabled={isLoading} id={id} onClick={onClick} type={type || 'button'} className={className ? `${classButton} ${className}` : classButton}>
+        <button disabled={isLoading} id={id} onClick={onClick} type={type || 'button'} className={className ? `${classButton} ${className} btn` : classButton}>
             {
                 isLoading ? <Loader /> : children
             }
