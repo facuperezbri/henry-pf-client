3d300d3383386b233448592307ff9b35a5ee20ab
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-08-09 22:43:25 -0300

    about us styles

--- src/components/AboutUsComp/AboutUsComp.js
+++ src/components/AboutUsComp/AboutUsComp.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {RiGithubLine} from 'react-icons/ri'
-import {RiLinkedinBoxLine} from 'react-icons/ri'
+import { RiGithubLine } from 'react-icons/ri'
+import { RiLinkedinBoxLine } from 'react-icons/ri'
 import Card from "../uiComponents/Card";
 import CardText from "../uiComponents/CardText";
 import agusPic from '../../assets/img/TeamPics/Agus.jpeg'
@@ -13,56 +13,56 @@ import mateoPic from '../../assets/img/TeamPics/Mateo.jpeg'
 import matiPic from '../../assets/img/TeamPics/Mati.jpeg'
 
 
-export default function AboutUsComp() {
+export default function AboutUsComp () {
 
     const team = [
-        {index: 0, name:'Agustín', lastname:'Vázquez', github:'https://github.com/Agutricolor', linkedin:'https://www.linkedin.com/in/agustin-v%C3%A1zquez-a939771b0/', photo: agusPic, description:'Descripción de Agus'},
-        {index: 1, name:'Alberto', lastname:'Díaz Lugo', github:'https://github.com/andresDiazLugo', linkedin:'https://www.linkedin.com/in/alberto-andres-diaz-lugo-91b0a0239', photo: alberPic, description:'Descripción de Alber'},
-        {index: 2, name:'Diego Alonso', lastname:'Gutierrez', github:'https://github.com/DiegoAlonsoG', linkedin:'https://www.linkedin.com/in/diegoalonsogm/', photo: diegoPic, description:'Descripción de Diego'},
-        {index: 3, name:'Facundo', lastname:'Pérez Brizuela', github:'https://github.com/facuperezbri', linkedin:'https://www.linkedin.com/in/facuperezbri/', photo:facuPic, description:'Descripción de Facu'},
-        {index: 4, name:'Franco', lastname:'Maciel', github:'https://github.com/FrM-bot', linkedin:'https://www.linkedin.com/in/franco-maciel', photo: francoDPic, description:'Descripción de FrancoDamian'},
-        {index: 5, name:'Franco', lastname:'Meinardo', github:'https://github.com/francomei', linkedin:'https://www.linkedin.com/in/franco-meinardo/', photo: francoMPic, description:'Descripción de FrancoMei'},
-        {index: 6, name:'Mateo', lastname:'Capdevila Ontivero', github:'https://github.com/mateocpd', linkedin:'https://www.linkedin.com/in/mateocapdevila', photo: mateoPic, description:'Descripción de Mateo'},
-        {index: 7, name:'Matías', lastname:'Britez', github:'https://github.com/mattbritez7', linkedin:'https://www.linkedin.com/in/matias-britezdev/', photo: matiPic, description:'Descripción de Matt'}
+        { index: 0, name: 'Agustín', lastname: 'Vázquez', github: 'https://github.com/Agutricolor', linkedin: 'https://www.linkedin.com/in/agustin-v%C3%A1zquez-a939771b0/', photo: agusPic, description: 'Descripción de Agus' },
+        { index: 1, name: 'Alberto', lastname: 'Díaz Lugo', github: 'https://github.com/andresDiazLugo', linkedin: 'https://www.linkedin.com/in/alberto-andres-diaz-lugo-91b0a0239', photo: alberPic, description: 'Descripción de Alber' },
+        { index: 2, name: 'Diego Alonso', lastname: 'Gutierrez', github: 'https://github.com/DiegoAlonsoG', linkedin: 'https://www.linkedin.com/in/diegoalonsogm/', photo: diegoPic, description: 'Descripción de Diego' },
+        { index: 3, name: 'Facundo', lastname: 'Pérez Brizuela', github: 'https://github.com/facuperezbri', linkedin: 'https://www.linkedin.com/in/facuperezbri/', photo: facuPic, description: 'Descripción de Facu' },
+        { index: 4, name: 'Franco', lastname: 'Maciel', github: 'https://github.com/FrM-bot', linkedin: 'https://www.linkedin.com/in/franco-maciel', photo: francoDPic, description: 'Descripción de FrancoDamian' },
+        { index: 5, name: 'Franco', lastname: 'Meinardo', github: 'https://github.com/francomei', linkedin: 'https://www.linkedin.com/in/franco-meinardo/', photo: francoMPic, description: 'Descripción de FrancoMei' },
+        { index: 6, name: 'Mateo', lastname: 'Capdevila Ontivero', github: 'https://github.com/mateocpd', linkedin: 'https://www.linkedin.com/in/mateocapdevila', photo: mateoPic, description: 'Descripción de Mateo' },
+        { index: 7, name: 'Matías', lastname: 'Britez', github: 'https://github.com/mattbritez7', linkedin: 'https://www.linkedin.com/in/matias-britezdev/', photo: matiPic, description: 'Descripción de Matt' }
     ]
 
-  return (
-    <section className='flex flex-col justify-center items-center mx-auto my-auto'>
-        <h1 className='mb-10 text-4xl bg-red-400 p-5 rounded-3xl'>Meet the team!</h1>
-        <ul className='grid grid-cols-1 md:grid-cols-2 gap-x-24 gap-y-10'>
-        {
-            team.map((member, i) => (
-                <li className='flex' key={i}>
+    return (
+        <section className='flex flex-col justify-center items-center mx-auto my-auto p-5'>
+            <h1 className='mb-10 text-4xl bg-red-400 p-5 rounded-3xl'>Meet the team!</h1>
+            <ul className='grid grid-cols-1 md:grid-cols-2 gap-x-24 gap-y-10'>
+                {
+                    team.map((member, i) => (
+                        <li className='flex' key={i}>
 
-                    <Card className="flex w-full">
+                            <Card className="flex w-full">
 
-                        <div className="flex flex-col gap-2">
-                            <div className='grid w-96 grid-cols-1 md:grid-cols-2 gap-2'>
-                                <CardText>
-                                    <div className='flex flex-col'>
-                                    <span>
-                                    {member.name}
-                                    </span>
-                                    <span>
-                                    {member.lastname}
-                                    </span>
+                                <div className="flex flex-col gap-2 justify-center">
+                                    <div className='grid w-96 grid-cols-1 md:grid-cols-2 gap-2'>
+                                        <CardText>
+                                            <div className='flex flex-col'>
+                                                <span>
+                                                    {member.name}
+                                                </span>
+                                                <span>
+                                                    {member.lastname}
+                                                </span>
+                                            </div>
+                                        </CardText>
+                                        <div className='flex w-40 flex-wrap items-center justify-center'>
+                                            <a href={member.github} target='_blank'><RiGithubLine size={40} /></a>
+                                            <a href={member.linkedin} target='_blank'><RiLinkedinBoxLine size={40} /></a>
+                                        </div>
                                     </div>
-                                </CardText>
-                                <div className='flex w-40 flex-wrap items-center justify-center'> 
-                                <a href={member.github} target='_blank'><RiGithubLine size={40}/></a>
-                                <a href={member.linkedin} target='_blank'><RiLinkedinBoxLine size={40}/></a>
+                                    <p>{member.description}</p>
                                 </div>
-                            </div>
-                            <p>{member.description}</p>
-                        </div>
 
-                        <img className='w-32 h-32 rounded-full' src={member.photo} alt={member.name}/>
-                    
-                    </Card>
-                </li>
-            ))
-        }
-        </ul>
-    </section>
-  )
+                                <img className='w-32 h-32 rounded-full' src={member.photo} alt={member.name} />
+
+                            </Card>
+                        </li>
+                    ))
+                }
+            </ul>
+        </section>
+    )
 }
