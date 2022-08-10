d7c5b040966867e9ba9b4e13c4265d490497916a
Matt <maty.britez7@gmail.com>
2022-08-10 12:20:03 -0300

    add

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,7 +1,7 @@
 import UsersToApprove from './UsersToAprove'
-import { Link } from 'react-router-dom'
+
 import BanSection from './BanSection'
-import Button from '../../components/uiComponents/Button'
+
 import Nav from '../../components/Nav/Nav'
 
 const Admin = () => {
@@ -10,10 +10,7 @@ const Admin = () => {
       <Nav />
 
       <div className='flex flex-col w-full max-w-screen-2xl py-8 px-8'>
-     
-      <Link to="/home">
-        <Button>Go home</Button>
-      </Link>
+    
 
       <BanSection />
       <UsersToApprove />
