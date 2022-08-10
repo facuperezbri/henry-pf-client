fb6091ab87acebcd59e8c9d0c194e1db01974bca
Diego Alonso <diegoalonso.gutierrez@outlook.com>
2022-08-09 22:38:57 -0500

    Refactor nav to include admin dashboard to admins

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -2,11 +2,14 @@ import UsersToApprove from './UsersToAprove'
 import { Link } from 'react-router-dom'
 import BanSection from './BanSection'
 import Button from '../../components/uiComponents/Button'
+import Nav from '../../components/Nav/Nav'
 
 const Admin = () => {
   return (
-    <div className='min-h-screen flex justify-center py-8'>
-      <div className='flex flex-col max-w-screen-2xl'>
+    <div className='min-h-screen flex'>
+      <Nav />
+
+      <div className='flex flex-col max-w-screen-2xl m-auto'>
      
       <Link to="/home">
         <Button>Go home</Button>
